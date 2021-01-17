04b16474a0b1280e479f29e609959ff9d319b71a
Windy Feng <1745500559@qq.com>
2021-01-17 09:47:01 +0800

    Create VeriCodeSMSAndCallContentGenerator.php

---
+++ src/Communication/CommunicationContents/Interfaces/VeriCodeSMSAndCallContentGenerator.php
@@ -0,0 +1,18 @@
+<?php
+namespace InteractivePlus\PDK2021Core\Communication\CommunicationContents\Interfaces;
+
+use InteractivePlus\PDK2021Core\Communication\CommunicationContents\Interfaces\EmailContent;
+use InteractivePlus\PDK2021Core\Communication\VerificationCode\VeriCodeEntity;
+use InteractivePlus\PDK2021Core\User\UserInfo\UserEntity;
+
+interface VeriCodeEmailContentGenerator{
+    public function getContentForPhoneVerification(VeriCodeEntity $veriCodeEntity, UserEntity $relatedUser) : string;
+    public function getContentForImportantAction(VeriCodeEntity $veriCodeEntity, UserEntity $relatedUser) : string;
+    public function getContentForChangePassword(VeriCodeEntity $veriCodeEntity, UserEntity $relatedUser) : string;
+    public function getContentForForgetPassword(VeriCodeEntity $veriCodeEntity, UserEntity $relatedUser) : string;
+    public function getContentForChangeEmail(VeriCodeEntity $veriCodeEntity, UserEntity $relatedUser) : string;
+    public function getContentForChangePhone(VeriCodeEntity $veriCodeEntity, UserEntity $relatedUser) : string;
+    public function getContentForAdminAction(VeriCodeEntity $veriCodeEntity, UserEntity $relatedUser) : string;
+    public function getContentForThirdAPPImportantAction(VeriCodeEntity $veriCodeEntity, UserEntity $relatedUser) : string;
+    public function getContentForThirdAPPDeleteAction(VeriCodeEntity $veriCodeEntity, UserEntity $relatedUser) : string;
+}
