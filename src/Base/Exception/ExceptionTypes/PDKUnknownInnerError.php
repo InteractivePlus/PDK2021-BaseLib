afb3e46e3bf018b4f389f04c1821c45948b3e4a4
Windy Feng <1745500559@qq.com>
2021-01-18 10:13:11 +0800

    Update PDKUnknownInnerError.php

--- src/Base/Exception/ExceptionTypes/PDKUnknownInnerError.php
+++ src/Base/Exception/ExceptionTypes/PDKUnknownInnerError.php
@@ -4,7 +4,7 @@ namespace InteractivePlus\PDK2021Core\Base\Exception\ExceptionTypes;
 use InteractivePlus\PDK2021Core\Base\Exception\PDKException;
 use InteractivePlus\PDK2021Core\Base\Exception\PDKErrCode;
 
-class PDKInnerError extends PDKException{
+class PDKUnknownInnerError extends PDKException{
     public function __construct(string $message = '', ?array $errParams = null, ?\Exception $previous = null){
         parent::__construct(PDKErrCode::UNKNOWN_INNER_ERROR,$message,$errParams,$previous);
     }
