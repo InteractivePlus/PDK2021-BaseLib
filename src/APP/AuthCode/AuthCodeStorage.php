58a82db48e5696eb4405bda63aeffe603c02875b
Windy Feng <1745500559@qq.com>
2021-02-01 19:26:54 +0800

    More Bugfix (urhhh)

--- src/APP/AuthCode/AuthCodeStorage.php
+++ src/APP/AuthCode/AuthCodeStorage.php
@@ -8,7 +8,7 @@ use InteractivePlus\PDK2021Core\Base\DataOperations\MultipleResult;
 abstract class AuthCodeStorage{
     protected abstract function __addAuthCodeEntity(AuthCodeEntity $entity) : void;
     public abstract function checkAuthCodeExist(string $authCode) : bool;
-    public abstract function getAuthCodeEntity(string $authCode) : ?AuthCodeStorage;
+    public abstract function getAuthCodeEntity(string $authCode) : ?AuthCodeEntity;
     public abstract function updateAuthCodeEntity(AuthCodeEntity $entity) : void;
     public abstract function useAuthCode(string $authCode) : void;
     public abstract function searchAuthCodeEntity(?string $authCode = null, int $createTimeStart = -1, int $createTimeEnd = -1, int $expireTimeStart = -1, int $expireTimeEnd = -1, ?string $relatedMaskID = null, int $relatedAPPUID = APPSystemConstants::NO_APP_RELATED_APPUID, int $dataOffset = 0, int $dataCountLimit = -1) : MultipleResult;
