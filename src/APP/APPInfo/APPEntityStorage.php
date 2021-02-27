e5b030eedc584b9ecca31311c44d753d0aebaa27
Windy Feng <1745500559@qq.com>
2021-02-27 21:50:34 +0800

    Update APPEntityStorage.php

--- src/APP/APPInfo/APPEntityStorage.php
+++ src/APP/APPInfo/APPEntityStorage.php
@@ -28,6 +28,7 @@ abstract class APPEntityStorage{
     protected abstract function __updateAPPEntity(APPEntity $entity) : void;
     public abstract function searchAPPEntity(?string $displayName = null, int $createTimeStart = -1, int $createTimeEnd = -1, int $ownerUID = UserSystemConstants::NO_USER_RELATED_UID, int $dataOffset = 0, int $dataCountLimit = -1) : MultipleResult;
     public abstract function getAPPEntityCount(?string $displayName = null, int $createTimeStart = -1, int $createTimeEnd = -1, int $ownerUID = UserSystemConstants::NO_USER_RELATED_UID) : int;
+    public abstract function deleteAPPEntity(int $appuid) : void;
     public function updateAPPEntity(APPEntity $entity) : void{
         $checkRst = $this->checkClientIDExist($entity->getClientID());
         if($checkRst !== -1 && $checkRst !== $entity->getAPPUID()){
