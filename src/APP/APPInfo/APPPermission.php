5a0a30cb789042b72d275ea2bb22defc550dea14
Windy Feng <1745500559@qq.com>
2021-05-03 21:14:19 +0800

    Edit APPPermission

--- src/APP/APPInfo/APPPermission.php
+++ src/APP/APPInfo/APPPermission.php
@@ -8,7 +8,9 @@ class APPPermission{
     public int $maxDataRecursionLevel;
     public int $maxCompressedDataLength;
     public int $maxUncompressedDataLength;
-    public int $maxDataRecordNumber; 
+    public int $maxDataRecordNumber;
+    public bool $canReadUserRealData;
+    public bool $isOfficialAPP;
     public array $allowedScopes;
     
     public function __construct(
@@ -16,7 +18,9 @@ class APPPermission{
         int $maxCompressedDataLength = 256, 
         int $maxUncompressedDataLength = 256*2, 
         int $maxDataRecordNumber = 1000, //-1 = Unlimited
-        array $allowedScopes = array('info','store_data')//array(APPTokenScopes::SCOPE_BASIC_INFO()->getScopeName(),APPTokenScopes::SCOPE_STORE_DATA()->getScopeName())
+        array $allowedScopes = array('info','store_data'), //array(APPTokenScopes::SCOPE_BASIC_INFO()->getScopeName(),APPTokenScopes::SCOPE_STORE_DATA()->getScopeName())
+        bool $canReadUserRealData = false,
+        bool $isOfficialAPP = false 
     )
     {
         $this->maxDataRecursionLevel = $maxDataRecursionLevel;
@@ -24,6 +28,8 @@ class APPPermission{
         $this->maxUncompressedDataLength = $maxUncompressedDataLength;
         $this->maxDataRecordNumber = $maxDataRecordNumber;
         $this->allowedScopes = $allowedScopes;
+        $this->canReadUserRealData = $canReadUserRealData;
+        $this->isOfficialAPP = $isOfficialAPP;
     }
     public function allowScope(string $scope) : bool{
         return in_array(strtolower($scope),$this->allowedScopes);
@@ -70,37 +76,49 @@ class APPPermission{
             'maxCompressedLen' => $this->maxCompressedDataLength,
             'maxUncompressedLen' => $this->maxUncompressedDataLength,
             'maxDataRecordNum' => $this->maxDataRecordNumber,
-            'scopes' => $this->allowedScopes
+            'scopes' => $this->allowedScopes,
+            'canReadUserRealData' => $this->canReadUserRealData,
+            'isOfficial' => $this->isOfficialAPP
         );
     }
     public static function fromAssocArray(array $data) : APPPermission{
-        $maxRLevel = 3;
-        $maxCompressedLen = 256;
-        $maxUncompressedLen = 256*2;
-        $maxRecordNum = 1000;
-        $scopes = array(APPTokenScopes::SCOPE_BASIC_INFO()->getScopeName());
+        $returnObj = new APPPermission();
         foreach($data as $dataKey => $dataVal){
             switch($dataKey){
                 case 'maxRecursionLevel':
-                    $maxRLevel = intval($dataVal);
+                    $returnObj->maxDataRecursionLevel = intval($dataVal);
                     break;
                 case 'maxCompressedLen':
-                    $maxCompressedLen = intval($dataVal);
+                    $returnObj->maxCompressedDataLength = intval($dataVal);
                     break;
                 case 'maxUncompressedLen':
-                    $maxUncompressedLen = intval($dataVal);
+                    $returnObj->maxUncompressedDataLength = intval($dataVal);
                     break;
                 case 'maxDataRecordNum':
-                    $maxRecordNum = intval($dataVal);
+                    $returnObj->maxDataRecordNumber = intval($dataVal);
                     break;
                 case 'scopes':
                     if(is_array($dataVal)){
-                        $scopes = $dataVal;
+                        $returnObj->allowedScopes = $dataVal;
+                    }
+                    break;
+                case 'canReadUserRealData':
+                    if($dataVal && $dataVal !== false && strtolower($dataVal) !== 'false'){
+                        $returnObj->canReadUserRealData = true;
+                    }else{
+                        $returnObj->canReadUserRealData = false;
+                    }
+                    break;
+                case 'isOfficial':
+                    if($dataVal && $dataVal !== false && strtolower($dataVal) !== 'false'){
+                        $returnObj->isOfficialAPP = true;
+                    }else{
+                        $returnObj->isOfficialAPP = false;
                     }
                     break;
             }
         }
-        return new APPPermission($maxRLevel,$maxCompressedLen,$maxUncompressedLen,$maxRecordNum,$scopes);
+        return $returnObj;
     }
     private static function checkMaxRecursionLevel(array $assocArray, int $maxLevel) : bool{
         if($maxLevel <= 1){
